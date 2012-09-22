5433754f4c556f645cf9da2b0086794888a60e68
Mario Peart <agile.ronin@gmail.com>
2012-09-22 17:18:57 -0400

    creation of rails app demoapp

---
+++ demoapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoapp::Application.config.secret_token = 'f12aa530f01a230f8ceff9834e105508edb3c0c6f2914ea5a8b9faf4e8026d96cd43fbd18ac54447812480413a4098dc67c7e39eb6ab11162bbb979a673a657c'
